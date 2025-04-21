c3c9a1ace4a48115c2fbab9b8e59112ea1bddd0a
hienonichan <hvu6582@gmail.com>
2025-04-21 19:56:13 +0700

    delete DataInValidException, it is already caught global

--- src/main/java/single/project/e_commerce/exceptions/DataInvalidException.java
+++
@@ -1,9 +0,0 @@
-package single.project.e_commerce.exceptions;
-
-import single.project.e_commerce.utils.enums.ErrorCode;
-
-public class DataInvalidException extends RuntimeException {
-    public DataInvalidException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-    }
-}
